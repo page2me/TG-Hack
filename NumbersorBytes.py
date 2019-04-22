7965811331c8121ebc77f6fadd942e0348ac7f87
page2me kitarotao <page2me@msn.com>
2019-04-23 00:33:31 +0700

    Update NumbersorBytes.py

--- NumbersorBytes.py
+++ NumbersorBytes.py
@@ -4,6 +4,7 @@ import hashlib
 from Crypto.Util.number import long_to_bytes
 from Crypto.Util.number import bytes_to_long
 import binascii
+import time
 
 r = remote('bytes.tghack.no', 2010)
 i = 0
@@ -13,23 +14,16 @@ while True:
       msg = r.recv()
    except EOFError as e:
       print msg
-#      msg = r.recv()
-      print e
-#      continue
+      break
    lines = msg.splitlines()
-   print l
-#   print lines[0+i]
-#   print lines[1+i]
    if lines[0+i] == 'Here\'s a number:':
-      result = long_to_bytes(lines[1+i])
-#      print binascii.hexlify(result)+"\n"
+      result = long_to_bytes(lines[1+i].encode())
       r.send(binascii.hexlify(result)+"\n")
    if lines[0+i] == 'Here\'s some bytes:':
-      result = bytes_to_long(binascii.unhexlify(lines[1+i]))
-#      print str(result)+"\n"
+      result = bytes_to_long(binascii.unhexlify(lines[1+i].encode()))
       r.send(str(result)+"\n")
    i = 1
    l = l + 1
    if l > 1001:
       break
-#print binascii.hexlify(result)+"\n"
+print msg
